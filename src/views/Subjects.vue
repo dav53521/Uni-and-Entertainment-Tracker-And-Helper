ac9e4dfe19ed2223e1881624678f3e385d1a951f
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-01-03 02:33:58 +1100

    Created link to subject-details page

--- src/views/Subjects.vue
+++ src/views/Subjects.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
 <template>
     <h1>This is the Subjects page</h1>
+    <RouterLink to="/SubjectDetails" class="subject-details-routerlink">Subject Details</RouterLink>
 </template>
 
 <style scoped>
