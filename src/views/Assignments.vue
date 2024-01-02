3ff496c653ef6b753dd3c306c54c3c06ae38278d
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-01-03 02:43:22 +1100

    Created list of due assignments

--- src/views/Assignments.vue
+++ src/views/Assignments.vue
@@ -1,8 +1,19 @@
 <template>
     <main>
         <h1>This is the assignments page</h1>
+        <div class="due-assignments">
+            <h2 class="assignments-due-header">Assignments that are due</h2>
+            <ul class="assignment-due-list">
+            </ul>
+        </div>
     </main>
 </template>
 
 <style scoped>
+    h1 {
+        font-size: 40px;
+    }
+    .assignments-due-header {
+        font-size: 30px;
+    }
 </style>
