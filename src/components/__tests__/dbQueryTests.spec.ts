70322012fccbd6201e72e4593a52e3885b2c170e
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-01-04 15:59:45 +1100

    created test file for dbQuery

---
+++ src/components/__tests__/dbQueryTests.spec.ts
@@ -0,0 +1,11 @@
+import dbQueryTest from '@/dbQuery'
+import { describe, it, expect } from 'vitest'
+
+
+const query = new dbQueryTest('test-uni-and-entertainment-tracker-and-helper', '', '')
+
+describe('dbQuery', () => {
+    it('isAbleToConnect', () => {
+        
+    })
+})
